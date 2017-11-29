386dd88fbe8ebaa17985e392c826afdff3780404
Juho Enala <enaladev@gmail.com>
2017-11-29 16:16:05 +0200

    Added initial versions of crypto and rsautil files

---
+++ crypto.js
@@ -0,0 +1,73 @@
+var forge = require('node-forge');
+var pki = forge.pki;
+var rsa = pki.rsa;
+
+const AES_STANDARD = 'AES-CBC';
+
+
+class Crypto {
+
+    rsa: undefined;
+
+    constructor() {
+        this.rsa = new RSAUtil();
+        this.rsa.init().then((ready) => {
+            console.log(ready);
+        });
+    }
+
+    encrypt(message) {
+        var payload = {};
+
+        // Generate random keys
+        var key = forge.random.getBytesSync(32);
+        var iv = forge.random.getBytesSync(32);
+
+        // Encrypt random keys
+        var encryptedKey = this.rsa.encrypt(key);
+        var encryptedIv = this.rsa.encrypt(iv);
+
+        // Attach encrypted keys into payload
+        payload.key = forge.util.encode64(encryptedKey);
+        payload.iv = forge.util.encode64(encryptedIv);
+
+        // Create buffer and cipher
+        var buffer = forge.util.createBuffer(message, 'utf8');
+        var cipher = forge.cipher.createCipher(AES_STANDARD, key);
+
+        // Encryption
+        cipher.start({iv: iv});
+        cipher.update(buffer);
+        cipher.finish();
+
+        // Attach encrypted message int payload
+        payload.cipher = forge.util.encode64(cipher.output.data);
+
+        return JSON.stringify(payload)
+    }
+
+    decrypt(encrypted) {
+        var payload = JSON.parse(encrypted);
+
+        // Get encrypted keys and encrypted message from the payload
+        var encryptedKey = forge.util.decode64(payload.key);
+        var encryptedIv = forge.util.decode64(payload.iv);
+        var cipher = forge.util.decode64(payload.cipher);
+
+        // Use RSA to decrypt keys
+        var key = this.rsa.decrypt(encryptedKey);
+        var iv = this.rsa.decrypt(encryptedIv);
+        
+        // Create buffer and decipher
+        var buffer = forge.util.createBuffer(cipher);
+        var decipher = forge.cipher.createDecipher(AES_STANDARD, key);
+
+        // Descryption
+        decipher.start({iv: iv});
+        decipher.update(buffer);
+        decipher.finish()
+
+        // Return decrypted message
+        return decipher.output.data
+    }
+}
---
+++ rsautil.js
@@ -0,0 +1,88 @@
+import {AsyncStorage} from 'react-native';
+var forge = require('node-forge');
+var pki = forge.pki;
+var rsa = pki.rsa;
+
+const KEYSTORE_NAME = 'keyStore';
+const KEYPAIR_NAME = 'keypair';
+
+const RSA_KEY_SIZE = 4096;
+const RSA_STANDARD = 'RSAES-PKCS1-V1_5';
+
+class RSAUtil {
+
+    publicKey: undefined;
+    privateKey: undefined;
+    readyResolve: undefined;
+    ready: false;
+
+    init() {
+        return new Promise((resolve, reject) => {
+            this.readyResolve = resolve;
+            this.readKeys().then((success) => {
+                if (success) {
+                    this.readyResolve(true);
+                } else {
+                    this.generateKeypair();
+                }
+            });
+        });
+    }
+
+    generateKeypair() {
+        return new Promise((resolve, reject) => {
+
+            var done = (err, keypair) => {
+                this.saveKeys(keypair).then(() => {
+                    this.keypair = keypair;
+                    this.readyResolve(true);
+                })
+            }
+
+            rsa.generateKeyPair({bits: RSA_KEY_SIZE}, done)
+        });
+    }
+
+    saveKeys(keypair) {
+        return new Promise((resolve, reject) => {
+            pems = this.keysToPem(keypair);
+            AsyncStorage.setItem(`@${KEYSTORE_NAME}:${KEYPAIR_NAME}`, JSON.stringify(pems));
+            resolve(true)
+        });
+    }
+
+    readKeys() {
+        return new Promise((resolve, reject) => {
+            AsyncStorage.getItem(`@${KEYSTORE_NAME}:${KEYPAIR_NAME}`).then((keys) => {
+
+                if (keys) {
+                    var keypair = JSON.parse(keys);
+                    this.keypair = this.pemToKeys(keypair);
+                    resolve(true);
+                } else {
+                    resolve(false);
+                }
+            });
+        });
+    }
+
+    keysToPem(keypair) {
+        keypair.publicKey = pki.publicKeyToPem(keypair.publicKey);
+        keypair.privateKey = pki.privateKeyToPem(keypair.privateKey);
+        return keypair
+    }
+
+    pemToKeys(keypair) {
+        keypair.publicKey = pki.publicKeyFromPem(keypair.publicKey);
+        keypair.privateKey = pki.privateKeyFromPem(keypair.privateKey);
+        return keypair
+    }
+
+    encrypt(decrypted) {
+        return this.keypair.publicKey.encrypt(decrypted, RSA_STANDARD)
+    }
+
+    decrypt(encrypted) {
+        return this.keypair.privateKey.decrypt(encrypted, RSA_STANDARD)
+    }
+}
